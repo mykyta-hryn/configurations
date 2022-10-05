2017784fe37d4a92da0d6c1a381a533ee5cc277b
Mykyta Hryn <mykyta_hryn@epam.com>
2022-10-05 21:53:22 +0300

    fix: update Rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,13 @@
-import path from 'path';
-
-import alias from '@rollup/plugin-alias';
 import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import url from '@rollup/plugin-url';
-import svgr from '@svgr/rollup';
 import glob from 'glob';
 import del from 'rollup-plugin-delete';
 import ignoreImport from 'rollup-plugin-ignore-import';
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
 const jsConfig = {
-  input: glob.sync('src/**/index.js'),
+  input: glob.sync('src/index.js'),
   output: [
     {
       dir: 'dist/cjs',
@@ -33,29 +28,14 @@ const jsConfig = {
     ignoreImport({
       extensions: ['.css']
     }),
-    peerDepsExternal(),
-    alias({
-      entries: {
-        '@basics': path.resolve('./src/basics'),
-        '@components': path.resolve('./src/components'),
-        '@lib': path.resolve('./src/lib'),
-        '@assets': path.resolve('./src/assets')
-      }
-    }),
-    babel({
-      babelHelpers: 'bundled',
-      exclude: 'node_modules/**',
-      presets: ['@babel/preset-react']
-    }),
+    nodeResolve(),
     commonjs({
       include: /node_modules/
     }),
-    nodeResolve(),
     url(),
-    svgr({
-      icon: true,
-      titleProp: true,
-      exportType: 'default'
+    babel({
+      babelHelpers: 'bundled',
+      exclude: 'node_modules/**'
     })
   ]
 };
